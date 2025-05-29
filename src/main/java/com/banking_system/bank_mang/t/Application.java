f50a1568ef9c86175b9a5c41940bff431581b5b8
Tsedenya <emkedemealemtsedenya@gmail.com>
2025-05-29 17:00:37 +0300

    the bank management system with sspring boot

---
+++ src/main/java/com/banking_system/bank_mang/t/Application.java
@@ -0,0 +1,13 @@
+package com.banking_system.bank_mang.t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
